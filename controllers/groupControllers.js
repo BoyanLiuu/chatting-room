d036482acd5dc3565f9964aa8205d2c3706ef7d4
Boyan Liu <heyiamboyan@gmail.com>
2019-12-04 01:16:37 -0500

    add send user request controller method version 1

--- controllers/groupControllers.js
+++ controllers/groupControllers.js
@@ -1,12 +1,69 @@
-const Users= require('../models/userModel');
-
-exports.getGroupPage = function (req, res) {
+const Users = require('../models/userModel');
+const async = require("async");
+exports.getGroupPage = (req, res) => {
     let name = req.params.name
     let gpname = name.split("_").join(" ")
-    res.render('groupchat/group',{user:req.user,name,gpname})
-    
+    res.render('groupchat/group', {
+        user: req.user,
+        name,
+        gpname
+    })
+
 }
 
-exports.groupPostPage = function (req, res) {
+exports.groupPostPage = (req, res) => {
+    async.parallel([(callback) => {
+            if (req.body.receiver) {
+                Users.update({
+                    'username': req.body.receiver,
+                    'request.userId': {
+                        $ne: req.user._id
+                    },
+                    'friendsList.friendId': {
+                        $ne: req.user._id
+                    }
+                }, {
+                    $push: {
+                        request: {
+                            userId: req.user._id,
+                            username: req.user.username
+                        }
+                    },
+                    $inc: {
+                        totalRequest: 1
+                    }
+                }, (err, count) => {
+                    callback(err, count);
+                })
+            }
+
+        }, (callback) => {
+            if (req.body.receiver) {
+                Users.update({
+                        'username': req.body.receiver,
+                        'sentRequest.username': {
+                            $ne: req.body.receiver
+                        }
+                    }, {
+                        $push: {
+                            request: {
+                                userId: req.user._id,
+                                username: req.user.username
+                            }
+                        }
+                    }
+
+                    , (err, count) => {
+                        callback(err, count);
+
+                    });
+
+            }
+
+        }
+
+    ], (err, results) => {
+        res.redirect('/group/' + req.params.name);
+    });
 
 }
